63a8badd4395575c5155937680ed80a59ad4ff8a
MaxChuloshnikov <maksvayner@gmail.com>
2022-08-21 19:19:37 +0300

    project 0.01

---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+
+const Profile = () => {
+    return (
+        <div className='content'>
+    <img src='https://cdnb.artstation.com/p/assets/images/images/021/856/307/large/tarmo-juhola-laboratorio2-copy.jpg?1573252751'/>
+    <div>
+        avatar + discription
+      </div>
+      <div>
+        My posts
+        <div>
+          new post
+        </div>
+        <div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+      </div>
+    </div>
+      
+    );
+}
+
+export default Profile;
